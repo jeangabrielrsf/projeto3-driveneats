fdaa812c71eefda4a0d687dc61d4fcf22b372dd9
Jean Frusca <jeang96@gmail.com>
2022-06-13 02:12:57 -0300

    implementada a função de enviar mensagem para o whatsapp. Resta consertar os ícones verdes do item selecionado

--- scripts.js
+++ scripts.js
@@ -2,54 +2,96 @@
 let pratoSelecionado; //guarda o prato selecionado
 let bebidaSelecionado; //guarda a bebida selecionada
 let sobremesaSelecionado; //guarda a sobremesa selecionada
+let pratoValue = 0;
+let bebidaValue = 0;
+let sobremesaValue = 0;
+let habilitado = false;
 
 
 function selecionarPrato (elemento) {
     const verdinho = document.querySelector(".prato .selecionado");
-    pratoSelecionado = elemento.innerHTML;
+    const icone = document.querySelector(".container-preco .icone-check");
+    pratoSelecionado = elemento.querySelector(".item-titulo p").innerHTML;
+    pratoValue = elemento.querySelector(".preco p").innerHTML;
+
 
     if (verdinho !== null) {
+        console.log(icone);
+        icone.classList.add("escondido");
         verdinho.classList.remove("selecionado");
     }
 
+    console.log(icone);
+    icone.classList.remove("escondido");
     elemento.classList.add("selecionado");
-    fecharPedido();
+    habilitaBotao();
 }
 
 function selecionarBebida (elemento) {
     const verdinho = document.querySelector(".bebida .selecionado");
-    bebidaSelecionado = elemento.innerHTML;
+    bebidaSelecionado = elemento.querySelector(".item-titulo p").innerHTML;
+    bebidaValue = elemento.querySelector(".preco p").innerHTML;
+
 
     if (verdinho !== null) {
         verdinho.classList.remove("selecionado");
     }
 
     elemento.classList.add("selecionado");
-    fecharPedido();
+    habilitaBotao();
 }
 
 function selecionarSobremesa (elemento) {
     const verdinho = document.querySelector(".sobremesa .selecionado");
-    sobremesaSelecionado = elemento.innerHTML;
+    sobremesaSelecionado = elemento.querySelector(".item-titulo p").innerHTML;
+    sobremesaValue = elemento.querySelector(".preco p").innerHTML;
+
 
     if (verdinho !== null) {
         verdinho.classList.remove("selecionado");
     }
 
     elemento.classList.add("selecionado");
-    fecharPedido();
+    habilitaBotao();
 }
 
 
 
-function fecharPedido () {
+function habilitaBotao () {
     
     let botao = document.querySelector(".botao-barra-inferior");
 
     if (pratoSelecionado && bebidaSelecionado && sobremesaSelecionado) {
         botao.classList.add("botao-habilitado");
         botao.innerHTML = "Fechar pedido";
+        habilitado = true;
     }
 
+    
+}
+
+ 
+function fecharPedido () {
+
+    let precoTotal = somaPreco(pratoValue, bebidaValue, sobremesaValue);
+    let mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${pratoSelecionado}\n- Bebida: ${bebidaSelecionado}\n- Sobremesa: ${sobremesaSelecionado}\nTotal: R$ ${precoTotal}`;
+    let codificada = encodeURIComponent(mensagem);
+    
+
+    if (habilitado!== false) {
+        window.open(`https://wa.me/5521988743639?text=${codificada}`);
+    }
 
 }
+
+function somaPreco (string1, string2, string3) {
+
+    let num1 = Number(string1);
+    let num2 = Number(string2);
+    let num3 = Number(string3);
+
+    let resultado = (num1+num2+num3).toFixed(2);
+
+    return resultado;
+}
+
